122058b89e6b0ad1d6948a597de6a136ef01d522
Luis-Berrio <laberrio@unicesar.edu.co>
2024-12-30 14:36:52 -0500

    implementacion terminada

--- src/app/tournament/tournament-bracket/tournament-bracket.component.ts
+++ src/app/tournament/tournament-bracket/tournament-bracket.component.ts
@@ -1,10 +1,11 @@
 import { Component, Input } from '@angular/core';
 import { BattleResult } from '../../models/battle-result';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-tournament-bracket',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './tournament-bracket.component.html',
   styleUrl: './tournament-bracket.component.css'
 })
